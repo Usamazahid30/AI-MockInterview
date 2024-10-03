bcb2f95d72df783eeb4418ec42b8dea9b90bda71
Muhammad Usama/IT/HO <zahidusama20@gmail.com>
2024-10-03 16:19:12 +0500

    change bg in sign in screen

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -1,13 +1,16 @@
 import { SignIn } from "@clerk/nextjs";
+import Image from "next/image";
+import interviewbg from "../[[...sign-in]]/interview.jpg";
 
 export default function Page() {
   return (
     <section class="bg-white">
       <div class="lg:grid lg:min-h-screen lg:grid-cols-12">
         <section class="relative flex h-32 items-end bg-gray-900 lg:col-span-5 lg:h-full xl:col-span-6">
-          <img
+          <Image
             alt=""
-            src="https://images.unsplash.com/photo-1617195737496-bc30194e3a19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+            // src="https://images.unsplash.com/photo-1617195737496-bc30194e3a19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+            src={interviewbg}
             class="absolute inset-0 h-full w-full object-cover opacity-80"
           />
 
@@ -28,7 +31,7 @@ export default function Page() {
             </a>
 
             <h2 class="mt-6 text-2xl font-bold text-white sm:text-3xl md:text-4xl">
-              Welcome to PrepPro 🦑
+              Welcome to PrepPro
             </h2>
 
             <p class="mt-4 leading-relaxed text-white/90">
